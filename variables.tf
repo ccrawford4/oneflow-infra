ae8d4254282477330b50fc6ab4304d58cef445be
ccrawford4 <calumcrawford9@gmail.com>
2025-10-25 19:12:08 -0700

    update defaults

--- variables.tf
+++ variables.tf
@@ -14,6 +14,25 @@ variable "subnet_count" {
 variable "settings" {
   description = "Configuration settings"
   type        = map(any)
+  default = {
+    "database" = {
+      engine                 = "mysql"
+      name                   = "product"
+      engine_version         = "8.0.40"
+      skip_final_snapshot    = true
+      dev_allocated_storage  = 10
+      prod_allocated_storage = 20
+      dev_instance_type      = "db.t3.micro"
+      prod_instance_type     = "db.t4g.large"
+    },
+    "web_app" = {
+      dev_instance_type  = "t4g.nano"
+      prod_instance_type = "t4g.large"
+      count              = 1
+      source_ami_id      = "ami-09865693345bd0d76"
+      source_ami_region  = "us-east-2"
+    }
+  }
 }
 variable "public_subnet_cidr_blocks" {
   description = "Available CIDR blocks for public subnets"
@@ -43,18 +62,22 @@ variable "environment" {
 variable "db_username" {
     description = "The username for the database."
     type = string
+    default = "admin"
 }
 variable "db_password" {
     description = "The password for the database."
     type = string
+    default = "change-me-in-production"
 }
 variable "db_name" {
     description = "The name of the database"
     type = string
+    default = "oneflow_dev"
 }
 variable "db_port" {
     description = "The port number for the database"
     type = number
+    default = 3306
 }
 variable "aws_account_id" {
     description = "The AWS account ID."
